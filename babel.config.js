f8d6d767b30bcfa4cbd675d5fb72d1439b1f42e8
yangbo <1832064870@qq.com>
2026-02-11 13:32:47 +0800

    update

---
+++ babel.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  presets: [
+    'module:@react-native/babel-preset',
+    // !mark
+    'nativewind/babel'
+  ],
+};
---
+++ metro.config.js
@@ -0,0 +1,17 @@
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+// !mark
+const { withNativeWind } = require('nativewind/metro');
+
+/**
+ * Metro configuration
+ * https://reactnative.dev/docs/metro
+ *
+ * @type {import('@react-native/metro-config').MetroConfig}
+ */
+const config = mergeConfig(getDefaultConfig(__dirname), {
+  // customizing the default config
+});
+
+module.exports = withNativeWind(config, {
+  input: './global.css',
+});
